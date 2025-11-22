1b47b328f431d64658377083e649f8aeeb8e8dc7
Stein <86646466+IamSTEINI@users.noreply.github.com>
2025-11-22 11:51:58 +0100

    Svelte setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Footer from './Footer.svelte';
+	import Header from './Header.svelte';
+	import './layout.css';
+	
+	let { children } = $props();
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		{@render children()}
+	</main>
+
+	<Footer />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>SVELTE-UI</title>
+	<meta name="description" content="Svelte UI Framework" />
+</svelte:head>
+
+<section>
+	<p class="font-bold text-4xl">Test</p>
+</section>
